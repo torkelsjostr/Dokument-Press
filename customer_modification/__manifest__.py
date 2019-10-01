b46ee935e2d733297ddd61e425816eba4024387e
randikaar <Rena@123>
2019-10-01 10:49:10 +0530

    Alterations of customers

---
+++ customer_modification/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Customer Modifications',
+    'version': '1.0',
+    'category': 'Sales',
+    'sequence': 1,
+    'author': "Allion Technologies PVT Ltd",
+    'website': 'http://www.alliontechnologies.com/',
+    'summary': 'Adding new custom fields to the customer table',
+    'description': """Adding new fields & mapping some modification according to client's requirements""",
+    'depends': ['base',
+                'sale_management'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/customer_vat_type_view.xml',
+        'views/res_partners_modifications_view.xml',
+        'views/customer_groups_view.xml',
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ customer_modification/models/res_partners_modification.py
@@ -0,0 +1,37 @@
+from odoo import models, fields, api
+
+
+class ResCustomerModification(models.Model):
+    _inherit = 'res.partner'
+
+    customer_group = fields.Many2one('customer.group', string='Customer Group', translate=True)
+    customer_vat_type = fields.Many2one('customer.vat.type', string='Customer VAT Type', translate=True)
+    vat_of_the_client = fields.Char(string='The VAT of The Client', translate=True)
+    currency = fields.Many2one('res.currency', string='Currency', translate=True)
+    important_notes = fields.Text(string='Important Notes', translate=True)
+    email_2 = fields.Char(string='Primary contact: Email', translate=True)
+    primary_phone = fields.Char(string='Primary contact: Phone number', translate=True)
+    customer_code = fields.Char(string='Customer Code', translate=True)
+    type = fields.Selection(
+        [('contact', 'Contact'),
+         ('invoice', 'Billing address'),
+         ('delivery', 'Delivery address'),
+         ('other', 'Other address'),
+         ("private", "Private Address"),
+         ], string='Address Type',
+        default='contact',
+        help="Used by Sales and Purchase Apps to select the relevant address depending on the context.")
+
+
+class CustomerGroups(models.Model):
+    _name = 'customer.group'
+
+    name = fields.Char(string='Customer Group Name', translate=True)
+    customer_group_id = fields.Char(string='ID', translate=True)
+
+
+class CustomerVatType(models.Model):
+    _name = 'customer.vat.type'
+
+    name = fields.Char(string='Customer VAT Type', translate=True)
+    customer_vat_id = fields.Char(string='ID', translate=True)
