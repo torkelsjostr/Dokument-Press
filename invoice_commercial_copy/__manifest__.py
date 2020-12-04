3e3e42db32927d46c8b891b28a8688d95f789364
torkelsjostr <53942440+torkelsjostr@users.noreply.github.com>
2020-12-04 09:30:35 +0100

    Add files via upload

---
+++ invoice_commercial_copy/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Odoo Preserve commercial in invoice copy",
+    'version': '12.0.0.0.1',
+    'depends': ['account'],
+    'author': "Coopdevs Treball SCCL",
+    'website': 'https://coopdevs.org',
+    'category': "Cooperative management",
+    'description': """
+    Odoo Preserve commercial in invoice copy.
+    """,
+    "license": "AGPL-3",
+    'data': [
+    ],
+}
