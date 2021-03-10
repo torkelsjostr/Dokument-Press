b2dc51beee384c411d84f1d1bb381f0704f5ab75
torkelsjostr <53942440+torkelsjostr@users.noreply.github.com>
2021-03-10 18:11:33 +0100

    Add files via upload

---
+++ sale_order_total_weight/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Sale Order Total Weight',
+    'version' : '1.1',
+    'summary': 'Sale Order Total Weight',
+    'sequence': 30,
+    'author': 'ErpMstar Solutions',
+    'description': "Sale Order Total Weight",
+    'category': 'Sales',
+    'depends' : ['web','sale'],
+    'data': [
+        'views/views.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [
+    ],
+    'images': [
+        'static/description/banner.jpg',
+    ],
+    'installable': True,
+    'website': '',
+    'auto_install': False,
+    'price': 15,
+    'currency': 'EUR',
+}
